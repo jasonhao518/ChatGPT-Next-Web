b8b55b5271061c0214c2f0a0958dfe086da0ccb8
jasonhao518 <jasonhao518@gmail.com>
2024-01-30 02:25:01 +0100

    Update common.ts

--- app/api/common.ts
+++ app/api/common.ts
@@ -43,7 +43,7 @@ export async function logTransaction(
   data: any,
 ) {
   const token = await getToken({ req });
-  const length = parseInt(req.headers.get("Content-Length")!) - 388;
+  const length = parseInt(req.headers.get("Content-Length")!) - 370;
   const transactionId = req.headers.get("x-transaction-id");
   if (token) {
     console.log(btoa(token.email!));
