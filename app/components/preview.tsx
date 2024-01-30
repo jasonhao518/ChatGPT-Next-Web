d5cc28b66ec3d54c7108b4f0a266d63933feb708
jasonhao518 <jasonhao518@gmail.com>
2024-01-30 12:05:37 +0100

    Update preview.tsx

--- app/components/preview.tsx
+++ app/components/preview.tsx
@@ -355,7 +355,12 @@ export function PreviewPage() {
               Array.from(
                 new Array(numPages > 10 ? 10 : numPages),
                 (el, index) => (
-                  <Page key={`page_${index + 1}`} pageNumber={index} />
+                  <Page
+                    key={`page_${index + 1}`}
+                    renderTextLayer={false}
+                    renderAnnotationLayer={false}
+                    pageNumber={index}
+                  />
                 ),
               )}
           </Document>
