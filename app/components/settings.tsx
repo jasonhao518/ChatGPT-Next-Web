94dd391ef472e3bd888e629bed863a21d9cd4d8f
jasonhao518 <jasonhao518@gmail.com>
2024-02-05 14:26:03 +0100

    restore sync

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -486,7 +486,7 @@ function SyncItems() {
   const promptStore = usePromptStore();
   const maskStore = useMaskStore();
   const couldSync = useMemo(() => {
-    return false;
+    return syncStore.coundSync();
   }, [syncStore]);
 
   const [showSyncConfigModal, setShowSyncConfigModal] = useState(false);
