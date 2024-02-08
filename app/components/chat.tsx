900be0a82c7d0a6aff6e4f27095bdedf3965b7fd
jasonhao518 <jasonhao518@gmail.com>
2024-02-08 13:18:56 +0100

    Update chat.tsx

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -141,7 +141,7 @@ function resizeImage(
 
               const resizedImage: ResizedImage = {
                 file: resizedFile,
-                base64: reader.result,
+                base64: reader.result as string,
               };
 
               resolve(resizedImage);
